e84c347a417a2de62440a80227e1c97871c76dbc
harirev <revathi139@gmail.com>
2025-04-22 15:27:34 +0530

    Updated number guessing game

--- projects/Number_guessing_game/main.py
+++ projects/Number_guessing_game/main.py
@@ -12,43 +12,51 @@ number = random.randint(1, 9)
 # into input box here number of
 # chances are 5
 chances = 0
+max_chances = 5
 
-print("Guess a number (between 1 and 9):")
+print("Guess a number (between 1 and 9).you have 5 chances:")
 
 # While loop to count the number
 # of chances
-while True:
+while chances < max_chances:
+    try:
 
     # Enter a number between 1 to 9
-    guess = int(input())
+        guess = int(input("Enter ur guess :"))
 
     # Compare the user entered number
     # with the number to be guessed
-    if guess == number:
+        if guess == number:
 
         # if number entered by user
         # is same as the generated
         # number by randint function then
         # break from loop using loop
         # control statement "break"
-        print(
+            print(
             f'CONGRATULATIONS! YOU HAVE GUESSED THE \
-            NUMBER {number} IN {chances} ATTEMPTS!')
+            NUMBER {number} IN {chances + 1} ATTEMPTS!')
         # Printing final statement using the f-strings method;
-        break
+            break
 
     # Check if the user entered
     # number is smaller than
     # the generated number
-    elif guess < number:
-        print("Your guess was too low: Guess a number higher than", guess)
+        elif guess < number:
+            print("Your guess was too low: Guess a number higher than", guess)
 
     # The user entered number is
     # greater than the generated
     # number
-    else:
-        print("Your guess was too high: Guess a number lower than", guess)
+        else:
+            print("Your guess was too high: Guess a number lower than", guess)
 
     # Increase the value of chance by 1
-    chances += 1
+        chances += 1
+
+    except ValueError:
+        print("Please enter a valid number.")
+
+if chances == max_chances and guess != number:
+    print(f"You have used all chances.")
     
